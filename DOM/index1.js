ba870506279a52dfe5df6ed9c57489cc8f25e7ea
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2022-12-06 14:14:24 +0600

    Till 106

--- DOM/index1.js
+++ DOM/index1.js
@@ -5,7 +5,7 @@ const third = document.getElementById('third')
 
 // Get class name
 const classValue = first.className
-console.log("Showing the class name:", classValue)
+console.log('Showing the class name:', classValue)
 
 // Gotcha: last added class will be active and others will remain inactive.
 second.className = 'colors'
@@ -45,10 +45,49 @@ result.appendChild(bodyDiv)
 console.log(result.children)
 
 // ----------------- 103 -----------------
+// insertBefore('element', 'location')
+const h2 = document.createElement('h2')
+const text1 = document.createTextNode('Prince')
+h2.append(text1)
+
+document.body.insertBefore(h2, result)
+
+// again (also work)
+// const red = document.getElementById('red')
+// result.insertBefore(h2, red)
+// big-element.insertBefore('created-element', 'location (before)')
+
 // ----------------- 104 -----------------
+// replaceChild('new', 'old')
+
+// creating new element
+const text2 = document.createTextNode('Hello Prince')
+const h3 = document.createElement('h3')
+h3.appendChild(text2)
+h3.classList.add('blue')
+
+document.body.replaceChild(h3, h2)
+
 // ----------------- 105 -----------------
+// prepend, innerText
+const heading = document.createElement('h2')
+heading.innerText = `I am a dynamic heading`
+document.body.prepend(heading)
+
 // ----------------- 106 -----------------
+// remove()
+// removeChild('removedElement')
+const demo = document.querySelector('#demo')
+const demoHeading1 = demo.querySelector('h1') // Search just inside the 'demo' (faster way) not the whole 'document'
+demoHeading1.remove() // remove that element
+
+const demoHeading2 = demo.querySelector('h2')
+demo.remove(demoHeading2)
+
 // ----------------- 107 -----------------
+// innerHTML, textContent
+
+
 // ----------------- 108 -----------------
 // ----------------- 109 -----------------
 // ----------------- 110 -----------------
